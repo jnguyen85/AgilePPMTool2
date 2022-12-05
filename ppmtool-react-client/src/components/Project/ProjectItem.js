1502810d978a8303e22fc9f6a4f23fb0a055741c
John <nguyen8585@gmail.com>
2022-12-05 11:28:37 -0800

    add react app project

---
+++ ppmtool-react-client/src/components/Project/ProjectItem.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from "react";
+
+class ProjectItem extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Project Item</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default ProjectItem;
